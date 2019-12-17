a72e4b5688016a6edf39764409383f4b04f77efd
Tharunkumar <tharunkumarsk@gmail.com>
2019-12-17 11:28:20 +0100

    Feat:Wrap the app with provider and pass store(middleware is pening)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 import "semantic-ui-css/semantic.min.css";
-
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./Reducers/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
