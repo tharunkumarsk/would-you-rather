2bb23665a9dc853d95752493f59d900762710ab6
Tharunkumar <tharunkumarsk@gmail.com>
2019-12-17 10:26:30 +0100

    Test:API is working properly by calling from componentdidmount

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,21 +6,33 @@ import NewQuestion from "./NewQuestion";
 import DashBoard from "./LeadersDashBoard";
 import HomePage from "./HomePage";
 import AnsweForQuestion from "./AnsweForQuestion";
+import { _getQuestions as getQuestions } from "../utils/_DATA";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Container>
-          <NavBar />
-          <Route exact path="/" component={HomePage} />
-          <Route path="/add" component={NewQuestion} />
-          <Route path="/leaderboard" component={DashBoard} />
-          <Route path="/questions/123" component={AnsweForQuestion} />
-        </Container>
-      </div>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  componentDidMount = () => {
+    getQuestions()
+      .then(questions => {
+        console.log(questions);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  };
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Container>
+            <NavBar />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/add" component={NewQuestion} />
+            <Route path="/leaderboard" component={DashBoard} />
+            <Route path="/questions/123" component={AnsweForQuestion} />
+          </Container>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
