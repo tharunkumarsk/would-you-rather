f2969fbd950394625bd08be25e1f7f4534935e04
Tharunkumar <tharunkumarsk@gmail.com>
2019-12-19 15:30:10 +0100

    Fix:_saveQuestionAnswer API expects authedUser instead of authUser

--- src/Actions/users.js
+++ src/Actions/users.js
@@ -1,6 +1,5 @@
 import { _saveQuestionAnswer } from "../utils/_DATA";
 import { saveAnswerToQuestion } from "./questionList";
-import { showLoading, hideLoading } from "react-redux-loading";
 
 export const RECEIVED_USERS = "RECEIVED_USERS";
 export const SAVE_ANSWER = "SAVE_ANSWER";
@@ -23,22 +22,11 @@ function saveAnswerToUser(authUser, qid, answer) {
 
 export function handleSaveAnswer(authUser, qid, answer) {
   return dispatch => {
-    //dispatch(showLoading());
     dispatch(saveAnswerToUser(authUser, qid, answer));
     dispatch(saveAnswerToQuestion(authUser, qid, answer));
 
-    return saveQuestionAnswer(authUser, qid, answer).catch(e => {
-      debugger;
-      //dispatch(hideLoading());
-      console.warn("Error in saving the answer ", e);
-      dispatch(saveAnswerToUser(authUser, qid, answer));
-      dispatch(saveAnswerToQuestion(authUser, qid, answer));
-      alert("Error in saving the answer. Try again.");
-    });
-  };
-}
+    const authedUser = authUser;
 
-export function saveQuestionAnswer(authUser, qid, answer) {
-  console.log("info", { authUser, qid, answer });
-  return _saveQuestionAnswer({ authUser, qid, answer });
+    return _saveQuestionAnswer({ authedUser, qid, answer });
+  };
 }
